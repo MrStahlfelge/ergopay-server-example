be9f0f26cdec150272a2b989b498fe3921455ecc
Benjamin Schulte <bs@golfgl.de>
2022-01-17 18:25:38 +0100

    Spring Initializr + Appkit Dependency

---
+++ src/main/java/org/ergoplatform/ergopay/ErgopayApplication.java
@@ -0,0 +1,13 @@
+package org.ergoplatform.ergopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErgopayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ergoplatform/ergopay/ErgopayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ergoplatform.ergopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
